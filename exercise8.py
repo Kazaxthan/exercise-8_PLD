33340e77940f0e193e1218596e2af38105290614
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-24 18:43:47 +0800

    third commit, here i added a code where it will ask the user a number where it will base it's rows.

--- exercise8.py
+++ exercise8.py
@@ -1,11 +1,14 @@
 #Exercise 8: Print the following pattern
 
+input_rows = int(input("type a number for how many rows you wanted.:"))
+if input_rows == str:
+     print("only numbers are allowed")
 
-for j in range (5):
+for j in range (1, input_rows + 1):
     
     for num in range(j):
 
 #print number
         print (j, end=" ") 
 #print result 
-    print()    
+    print("\n")    
